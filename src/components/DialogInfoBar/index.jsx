5721258f29616b1d92a8e51059eff58eaef7fb01
Saniyook <saniyook@gmail.com>
2019-11-21 00:13:20 +0300

    small last-seen info improvement

--- src/components/DialogInfoBar/index.jsx
+++ src/components/DialogInfoBar/index.jsx
@@ -14,16 +14,16 @@ const DialogInfoBar = ({ fullname, isOnline, currentDialog, last_seen }) => {
     else if (differenceInDays(new Date(), new Date(last_seen)) >= 2)
         lastSeen = `${differenceInDays(new Date(), new Date(last_seen))} days ago`
     else if (differenceInDays(new Date(), new Date(last_seen)) === 1)
-        lastSeen = `yesterday at ${format(new Date(last_seen), 'hh.mm a..aaa')}`
+        lastSeen = `yesterday at ${format(new Date(last_seen), 'h.mm aa')}`
     else if (differenceInHours(new Date(), new Date(last_seen)) >= 2)
         lastSeen = `${differenceInHours(new Date(), new Date(last_seen))} hours ago`
     else if (differenceInHours(new Date(), new Date(last_seen)) === 1)
         lastSeen = `${differenceInHours(new Date(), new Date(last_seen))} hour ago`
-    else if (differnceInMinutes(new Date(), new Date(last_seen)) > 10)
+    else if (differnceInMinutes(new Date(), new Date(last_seen)) > 15)
         lastSeen = `${differnceInMinutes(new Date(), new Date(last_seen))} minutes ago`
-    else
+    else if (differnceInMinutes(new Date(), new Date(last_seen)) > 2)
         lastSeen = 'recently'
-
+    else lastSeen = 'just now'
     return (
         currentDialog ? (<div className="chat__dialogInfo">
             <h3 className="chat__dialogInfo-userName">
